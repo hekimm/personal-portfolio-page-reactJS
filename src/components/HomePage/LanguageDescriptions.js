0c6d84f5db6326ad671aa56920d5f071ef0886e6
HEKİMCAN AKTAŞ <109082524+hekimm@users.noreply.github.com>
2023-10-03 03:00:45 +0300

    Updated  LanguageDescriptions.js for new sections of the HomePage Component

--- src/components/HomePage/LanguageDescriptions.js
+++ src/components/HomePage/LanguageDescriptions.js
@@ -223,4 +223,50 @@ Modularity: Node.js applications are modular, featuring a structure where core f
 Due to its non-blocking nature, Node.js is particularly suitable for tasks that require real-time processing or managing many concurrent connections, such as API servers, streaming services, and online gaming platforms.
     `,
   },
+
+  profileTitle: {
+    tr: "Full Stack Web Developer | Mobil Uygulama Geliştirici | English Master(C1)",
+    en: "Full Stack Web Developer | Mobile App Developer | English Master(C1)",
+  },
+  card1: {
+    tr: `Merhaba. Ben Hekimcan Aktaş, Kişisel gelişim sürecinin sürekli bir öğrenme ve yenilenme süreci olduğuna inanıyorum ve bu inançla, her yeni günü bir fırsat olarak görüp, bilgimi ve yeteneklerimi artırmaya devam ediyorum.
+
+    Portfolyoma Hoşgeldiniz. 
+
+    Yenilikçi Çözümlerle Dijital-Teknolojik Dönüşümde ve Gelişimde etkin rol alma hedefleriyle ilerliyorum.`,
+    en: `Hello. I am Hekimcan Aktaş. I believe that the personal development process is a continuous learning and renewal process, and with this belief, I see each new day as an opportunity to enhance my knowledge and skills.
+    
+    Welcome to my Portfolio.
+
+ I am progressing with the goals of actively playing a role in Digital-Technological Transformation and Development with Innovative Solutions.`,
+  },
+  card2: {
+    tr: `2021 yılından beri Yazılım ve Programlama ile ilgileniyorum, çocukluğumdan beri her zaman bilgisayar ve teknolojiye çok ilgiliyim ve bu konuda çalışmalar yapıyorum.
+    
+    Html ve Python ile başladığım Yazılım ve Programlama yolculuğumda bugün pek çok modern web teknolojileri (TypeScript, Angular, React, React Router, Redux, Next.js) ve mobil uygulama geliştirme teknolojilerine (React Native, Flutter) hakimiyetim var.`,
+    en: `Since 2021, I have been interested in Software and Programming. Since my childhood, I have always been very interested in computers and technology, and I have been working on this subject. In my Software and Programming journey, which I started with HTML and Python, today I have mastery over many modern web technologies (TypeScript, Angular, React, React Router, Redux, Next.js) and mobile application development technologies (React Native, Flutter).`,
+  },
+  card3: {
+    tr: `2017 TEOG Sınavı Türkiye 1.siyim.
+    
+    Övgü Terzibaşıoğlu Anadolu Lisesi'nden 2021 yılında 94 ortalamayla mezun oldum.
+    
+    Şu anda Manisa Celal Bayar Üniversitesi Yazılım Mühendisliği bölümünde 2. sınıf öğrencisiyim.
+    
+    Bölümümde aldığım eğitimin yanı sıra, bireysel olarak da sürekli öğrenme ilkesiyle yeni teknolojileri, dilleri ve araçları öğrenmeye devam ediyorum.`,
+    en: `I ranked first in Turkey in the 2017 TEOG Exam and graduated from Övgü Terzibaşıoğlu Anatolian High School in 2021 with an average of 94.
+    
+    Currently, I am a 2nd Grade student on  Software Engineering at Manisa Celal Bayar University.
+    
+    Alongside the education I receive in my department, I also persistently continue to learn new technologies, languages, and tools on an individual basis, adhering to the principle of continual learning.`,
+  },
+  projectsButton: {
+    en: "My Projects",
+    tr: "Projelerim",
+  },
+
+  startJourneyButton: {
+    tr: "Yazılım Yolculuğum Nasıl Başladı?",
+    en: "How Did My Software Journey Begin?", // İsterseniz başka dilleri de ekleyebilirsiniz.
+  },
 };
