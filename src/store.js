046b6004b7fd3dfeb2c06a26904b92abbd1f2cc2
HEKİMCAN AKTAŞ <109082524+hekimm@users.noreply.github.com>
2023-10-16 01:10:39 +0300

    Updated store.js for adding a reducer for mediumPosts.

--- src/store.js
+++ src/store.js
@@ -2,17 +2,22 @@ import { createStore, applyMiddleware, combineReducers } from "redux";
 import thunk from "redux-thunk";
 import projectsReducer from "./reducers/projectsReducer";
 import loadingReducer from "./reducers/loadingReducer";
-
+import typingReducer from "./reducers/typingSlice";
 import themeReducer from "./reducers/themeSlice"; // yeni import
 import themeToggleReducer from "./reducers/themeToggleSlice";
 import languageReducer from "./reducers/languageSlice";
+import messageReducer from "./reducers/messageSlice";
+import mediumPostsReducer from "./reducers/mediumPostsSlice"; // New import
 
 const rootReducer = combineReducers({
   projects: projectsReducer,
   loading: loadingReducer,
+  typing: typingReducer,
   theme: themeReducer, // yeni state slice
   themeToggle: themeToggleReducer,
   language: languageReducer,
+  message: messageReducer,
+  mediumPosts: mediumPostsReducer, // New state slice
 });
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
