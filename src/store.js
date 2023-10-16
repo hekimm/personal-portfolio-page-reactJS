023edc2c4e486cfcb6f5e6dfbc65b8c059dc98ce
HEKİMCAN AKTAŞ <109082524+hekimm@users.noreply.github.com>
2023-10-16 23:21:20 +0300

    Updated store.js for some issues about MedumPosts data fetching.

--- src/store.js
+++ src/store.js
@@ -1,4 +1,6 @@
 import { createStore, applyMiddleware, combineReducers } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // default olarak localStorage kullanır
 import thunk from "redux-thunk";
 import projectsReducer from "./reducers/projectsReducer";
 import loadingReducer from "./reducers/loadingReducer";
@@ -6,6 +8,7 @@ import typingReducer from "./reducers/typingSlice";
 import themeReducer from "./reducers/themeSlice"; // yeni import
 import themeToggleReducer from "./reducers/themeToggleSlice";
 import languageReducer from "./reducers/languageSlice";
+import messageReducer from "./reducers/messageSlice";
 import mediumPostsReducer from "./reducers/mediumPostsSlice"; // New import
 
 const rootReducer = combineReducers({
@@ -15,9 +18,28 @@ const rootReducer = combineReducers({
   theme: themeReducer, // yeni state slice
   themeToggle: themeToggleReducer,
   language: languageReducer,
+  message: messageReducer,
   mediumPosts: mediumPostsReducer, // New state slice
 });
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: [
+    "projects",
+    "loading",
+    "typing",
+    "theme",
+    "themeToggle",
+    "language",
+    "message",
+  ], 
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = createStore(persistedReducer, applyMiddleware(thunk));
+
+export const persistor = persistStore(store);
 
 export default store;
