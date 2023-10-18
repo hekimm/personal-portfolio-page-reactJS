6516e30891d3a35f71a30a0135d94646d906d9f9
HEKİMCAN AKTAŞ <109082524+hekimm@users.noreply.github.com>
2023-10-19 00:15:19 +0300

    Updated About component.

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -305,16 +305,6 @@ const About = () => {
                 </SkillIcon>
                 Node.js (Express.js)
               </Skill>
-              <Skill>
-                <SkillIcon color="#007396">
-                  <img
-                    src="https://www.vectorlogo.zone/logos/java/java-icon.svg"
-                    alt="Java"
-                    style={{ width: "60px", height: "60px" }}
-                  />
-                </SkillIcon>
-                Java
-              </Skill>
             </SkillsList>
           </Card>
         </Article>
@@ -352,26 +342,6 @@ const About = () => {
                   />
                 </SkillIcon>
                 React Native
-              </Skill>
-              <Skill>
-                <SkillIcon color="#0095D5">
-                  <img
-                    src="https://www.vectorlogo.zone/logos/kotlinlang/kotlinlang-icon.svg"
-                    alt="Kotlin"
-                    style={{ width: "60px", height: "60px" }}
-                  />
-                </SkillIcon>
-                Kotlin
-              </Skill>
-              <Skill>
-                <SkillIcon color="#FA7343">
-                  <img
-                    src="https://www.vectorlogo.zone/logos/swift/swift-icon.svg"
-                    alt="Swift"
-                    style={{ width: "60px", height: "60px" }}
-                  />
-                </SkillIcon>
-                Swift
               </Skill>
             </SkillsList>
           </Card>
