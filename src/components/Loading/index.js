a387709b907b8692187f021a6c8f088fb264353d
HEKİMCAN AKTAŞ <109082524+hekimm@users.noreply.github.com>
2023-10-21 17:44:24 +0300

    Updated Loading component for new loading animation.

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -1,4 +1,40 @@
 import styled, { keyframes } from "styled-components";
+import profileImage from "./profile-image.jpeg";
+const heartbeat = keyframes`
+  0% {
+    transform: translate(-50%, -50%) scale(1);
+  }
+  10% {
+    transform: translate(-50%, -50%) scale(1.05);
+  }
+  15% {
+    transform: translate(-50%, -50%) scale(1);
+  }
+  25% {
+    transform: translate(-50%, -50%) scale(1.05);
+  }
+  30% {
+    transform: translate(-50%, -50%) scale(1);
+  }
+  100% {
+    transform: translate(-50%, -50%) scale(1);
+  }
+`;
+z
+const ProfileImage = styled.img`
+  position: absolute;
+  width: 100px;
+  height: 100px;
+  object-fit: cover;
+  border-radius: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 10;
+  border: 3px solid #ffffff;
+  box-shadow:
+    0 0 25px #61dafb,
+    0 0 5px #61dafb;
+  animation: ${heartbeat} 1.5s infinite; // Heartbeat animasyonunu ekledik.
+`;
 
 const rotate = keyframes`
   0% {
@@ -15,12 +51,12 @@ const dashOne = keyframes`
     stroke-dashoffset: 0;
   }
   50% {
-    stroke-dasharray: 100, 150;
-    stroke-dashoffset: -30;
+    stroke-dasharray: 85, 150;
+    stroke-dashoffset: -40;
   }
   100% {
-    stroke-dasharray: 100, 150;
-    stroke-dashoffset: -130;
+    stroke-dasharray: 85, 150;
+    stroke-dashoffset: -140;
   }
 `;
 
@@ -29,13 +65,13 @@ const dashTwo = keyframes`
     stroke-dasharray: 1, 150;
     stroke-dashoffset: 0;
   }
-  50% {
+  70% {
     stroke-dasharray: 110, 150;
-    stroke-dashoffset: -40;
+    stroke-dashoffset: -50;
   }
   100% {
     stroke-dasharray: 110, 150;
-    stroke-dashoffset: -140;
+    stroke-dashoffset: -150;
   }
 `;
 
@@ -45,7 +81,7 @@ const LoadingContainer = styled.div`
   left: 0;
   width: 100%;
   height: 100%;
-  background-color: rgba(0, 0, 0, 0.8);
+  background-color: rgba(0, 0, 0, 0.9);
   display: flex;
   justify-content: center;
   align-items: center;
@@ -53,42 +89,43 @@ const LoadingContainer = styled.div`
 `;
 
 const Spinner = styled.svg`
-  animation: ${rotate} 2s linear infinite;
-  margin: -25px 0 0 -25px;
-  width: 50px;
-  height: 50px;
+  animation: ${rotate} 3.5s linear infinite;
+  margin: -100px 0 0 -100px;
+  width: 200px;
+  height: 200px;
 
   & .pathOne {
-    stroke: #61dafb;
+    stroke: #61dafb; // Mavi renk kullandık.
     stroke-linecap: round;
-    animation: ${dashOne} 1.5s ease-in-out infinite;
+    animation: ${dashOne} 1.9s ease-in-out infinite;
   }
 
   & .pathTwo {
-    stroke: #fff;
+    stroke: #ff6b6b; // Canlı bir kırmızı ton kullandık.
     stroke-linecap: round;
-    animation: ${dashTwo} 2s ease-in-out infinite;
+    animation: ${dashTwo} 2.3s ease-in-out infinite;
   }
 `;
 
 const Loading = () => (
   <LoadingContainer>
-    <Spinner viewBox="0 0 50 50">
+    <ProfileImage src={profileImage} alt="Profil Resmi" />
+    <Spinner viewBox="0 0 200 200">
       <circle
         className="pathOne"
-        cx="25"
-        cy="25"
-        r="20"
+        cx="100"
+        cy="100"
+        r="80"
         fill="none"
-        strokeWidth="2"
+        strokeWidth="4"
       ></circle>
       <circle
         className="pathTwo"
-        cx="25"
-        cy="25"
-        r="15"
+        cx="100"
+        cy="100"
+        r="60"
         fill="none"
-        strokeWidth="2"
+        strokeWidth="4"
       ></circle>
     </Spinner>
   </LoadingContainer>
