79b9619efef740dced0f792cf0edab639f5e4075
HEKİMCAN AKTAŞ <109082524+hekimm@users.noreply.github.com>
2023-11-16 20:02:39 +0300

    Updated Footer component for enhanced UI/UX design.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,6 +1,6 @@
 import React from "react";
-import styled from "styled-components";
-import { Link } from "react-router-dom";
+import styled, { css } from "styled-components";
+import { Link, useLocation } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useSelector } from "react-redux";
 import { selectLanguage } from "../../reducers/languageSlice";
@@ -12,12 +12,13 @@ import {
 } from "@fortawesome/free-brands-svg-icons";
 import { faXTwitter as faTwitter } from "@fortawesome/free-brands-svg-icons/faXTwitter";
 import {
-  faNewspaper,
-  faBook,
-  faProjectDiagram,
-  faLaptopCode,
+  faHome,
+  faUserAlt,
+  faBriefcase,
   faEnvelope,
+  faGraduationCap,
   faCode,
+  faBlog,
 } from "@fortawesome/free-solid-svg-icons";
 
 const FooterContainer = styled.footer`
@@ -29,6 +30,7 @@ const FooterContainer = styled.footer`
   bottom: 0;
   width: 100%;
   border-top: 1px solid #343a40;
+  z-index: 2;
 `;
 
 const SocialIcons = styled.div`
@@ -50,54 +52,72 @@ const SocialIcon = styled.a`
     color: #ffffff;
     transform: scale(1.2);
   }
-`;
-
-const RouteButtons = styled.div`
-  display: flex;
-  justify-content: center;
-  gap: 15px;
-  margin-bottom: 15px;
-  flex-wrap: wrap;
-
   @media (max-width: 768px) {
-    gap: 10px;
-    font-size: 12px;
+    font-size: 0;
   }
 `;
 
+const RouteButtons = styled.nav`
+  display: flex;
+  justify-content: space-around;
+  background-color: #282c34; // Same as the footer container for a cohesive look
+  padding: 10px 0; // Adequate padding for touch targets
+
+  @media (max-width: 768px) {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    height: 50px; // Slightly reduced for mobile
+    border-top: 2px solid #343a40; // Slightly thicker for a pronounced separation
+  }
+`;
+
+const activeButtonStyle = css`
+  color: #61dafb; // Active button color
+  svg {
+    transform: scale(1.25); // Make the active icon larger
+  }
+`;
+
+// Update the RouteButton styled component to use the props to determine if it's active
 const RouteButton = styled(Link)`
-  color: #d3d3d3;
+  color: #adb5bd;
   font-size: 14px;
-  transition: color 0.3s;
-  text-decoration: none;
+  transition:
+    color 0.3s,
+    transform 0.3s ease-in-out;
   display: flex;
   align-items: center;
+  justify-content: center;
 
   &:hover {
-    color: #ffffff;
+    color: #61dafb;
   }
 
   svg {
-    margin-right: 8px;
+    font-size: 24px;
   }
 
-  @media (max-width: 768px) {
-    font-size: 12px;
-    svg {
-      margin-right: 4px;
-      font-size: 12px;
-    }
-  }
+  ${(props) => props.isActive && activeButtonStyle}
 `;
 
 const FooterText = styled.p`
-  margin-bottom: 10px;
-  font-size: 14px;
+  color: #adb5bd;
+  font-size: 12px; // Smaller font size for a more discreet copyright message
+  padding: 8px 0; // Padding for spacing
+  background-color: #20232a; // Slightly darker to differentiate from main footer area
+  width: 100%;
+  @media (max-width: 768px) {
+    font-size: 0;
+  }
 `;
 
 const Footer = () => {
+  const location = useLocation(); // Hook to get the current location
   const language = useSelector(selectLanguage);
-
+  // Function to determine if the route is active
+  const isActive = (path) => location.pathname === path;
   const TEXTS = {
     tr: {
       ABOUT: "Hakkımda",
@@ -121,69 +141,46 @@ const Footer = () => {
   return (
     <FooterContainer>
       <RouteButtons>
-        <RouteButton to="/about">
-          <FontAwesomeIcon icon={faNewspaper} /> {TEXTS[language].ABOUT}
+        {/* Apply the active style conditionally based on the current route */}
+        <RouteButton
+          to="/about"
+          aria-label="About"
+          isActive={isActive("/about")}
+        >
+          <FontAwesomeIcon icon={faUserAlt} />
         </RouteButton>
-        <RouteButton to="/education">
-          <FontAwesomeIcon icon={faBook} /> {TEXTS[language].EDUCATION}
+        <RouteButton
+          to="/skills"
+          aria-label="Skills"
+          isActive={isActive("/skills")}
+        >
+          <FontAwesomeIcon icon={faCode} />
         </RouteButton>
-        <RouteButton to="/projects">
-          <FontAwesomeIcon icon={faProjectDiagram} /> {TEXTS[language].PROJECTS}
+        <RouteButton
+          to="/education"
+          aria-label="Education"
+          isActive={isActive("/education")}
+        >
+          <FontAwesomeIcon icon={faGraduationCap} />
         </RouteButton>
-        <RouteButton to="/blog">
-          <FontAwesomeIcon icon={faMediumM} /> {TEXTS[language].BLOG}
+        <RouteButton
+          to="/projects"
+          aria-label="Projects"
+          isActive={isActive("/projects")}
+        >
+          <FontAwesomeIcon icon={faBriefcase} />
         </RouteButton>
-        <RouteButton to="/expertise-area">
-          <FontAwesomeIcon icon={faLaptopCode} />{" "}
-          {TEXTS[language].EXPERTISE_AREA}
+        <RouteButton to="/blog" aria-label="Blog" isActive={isActive("/blog")}>
+          <FontAwesomeIcon icon={faBlog} />
         </RouteButton>
-        <RouteButton to="/contact">
-          <FontAwesomeIcon icon={faEnvelope} /> {TEXTS[language].CONTACT}
-        </RouteButton>
-        <RouteButton to="/skills">
-          <FontAwesomeIcon icon={faCode} /> {TEXTS[language].SKILLS}
+        <RouteButton
+          to="/contact"
+          aria-label="Contact"
+          isActive={isActive("/contact")}
+        >
+          <FontAwesomeIcon icon={faEnvelope} />
         </RouteButton>
       </RouteButtons>
-      <FooterText>
-        Copyright © 2023 Hekimcan AKTAŞ. All rights are reserved.
-      </FooterText>
-      <SocialIcons>
-        <SocialIcon
-          href="https://medium.com/@hekimcanaktas"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FontAwesomeIcon icon={faMediumM} />
-        </SocialIcon>
-        <SocialIcon
-          href="https://twitter.com/hekimmcann"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FontAwesomeIcon icon={faTwitter} />
-        </SocialIcon>
-        <SocialIcon
-          href="https://github.com/hekimm"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FontAwesomeIcon icon={faGithub} />
-        </SocialIcon>
-        <SocialIcon
-          href="https://www.linkedin.com/in/hekimcanaktas/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FontAwesomeIcon icon={faLinkedin} />
-        </SocialIcon>
-        <SocialIcon
-          href="mailto:hekimcanaktas@gmail.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FontAwesomeIcon icon={faGoogle} />
-        </SocialIcon>
-      </SocialIcons>
     </FooterContainer>
   );
 };
