18230da0f4772118240b3a80534a256d87547652
HEKİMCAN AKTAŞ <109082524+hekimm@users.noreply.github.com>
2023-12-11 15:09:54 +0300

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import HomePage from "./components/HomePage";
 import NotFound from "./components/NotFound";
 import Footer from "./components/Footer";
 import MediumPosts from "./components/MediumPosts/index";
-import CodeEditor from "./components/CodeEditor/index"; // Import CodeEditor component
+import CodeEditor from "./components/CodeEditor"; // Import CodeEditor component
 
 import { useSelector } from "react-redux";
 import { ROUTES } from "./components/Routes";
